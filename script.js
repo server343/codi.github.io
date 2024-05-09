b4b408d531becdb7cfadd7922cb77a36e0a41a56
Pol <123597761+server343@users.noreply.github.com>
2024-05-09 11:57:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Valor de desplazamiento predeterminado
+// Valor de desplazamiento predeterminado para letras
 let desplazamientoPredeterminado = 3;
 
 // Función para cambiar el valor de desplazamiento desde la consola
@@ -17,6 +17,14 @@ function decodificar() {
     document.getElementById('resultado').innerText = cifrar(palabra, -desplazamientoPredeterminado);
 }
 
+// Mapeo de cifrado y descifrado para números
+const mapeoNumeros = {
+    '0': '3', '1': '4', '2': '5', '3': '6', '4': '7', '5': '8', '6': '9', '7': '0', '8': '1', '9': '2'
+};
+const mapeoInversoNumeros = {
+    '3': '0', '4': '1', '5': '2', '6': '3', '7': '4', '8': '5', '9': '6', '0': '7', '1': '8', '2': '9'
+};
+
 function cifrar(palabra, desplazamiento) {
     const alfabeto = 'abcdefghijklmnopqrstuvwxyz';
     let resultado = '';
@@ -28,6 +36,8 @@ function cifrar(palabra, desplazamiento) {
             let newPos = (pos + desplazamiento + 26) % 26;
             let nuevoChar = alfabeto[newPos];
             resultado += mayuscula ? nuevoChar.toUpperCase() : nuevoChar;
+        } else if (char.match(/[0-9]/)) {
+            resultado += desplazamiento >= 0 ? mapeoNumeros[char] : mapeoInversoNumeros[char];
         } else {
             resultado += char;
         }
